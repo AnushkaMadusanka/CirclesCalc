eab9c0a502fc259823c219a0179f91c1a3d3698d
AppDevNotes <anushkamadusanka2016@gmail.com>
2019-06-11 22:13:18 -0700

    var and val

--- app/src/main/java/com/anushka/circles/MainActivity.kt
+++ app/src/main/java/com/anushka/circles/MainActivity.kt
@@ -2,11 +2,19 @@ package com.anushka.circles
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val subject = "Maths"
+        var marks = 80
+        var average = 76.8
+
+        Toast.makeText(applicationContext,"I got $marks for $subject . My Average was $average ",Toast.LENGTH_LONG).show()
+
     }
 }
