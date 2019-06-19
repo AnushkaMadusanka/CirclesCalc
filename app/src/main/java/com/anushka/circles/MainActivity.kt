3a707c4562d46fe6448b3a984b5abec7e6dedb49
AppDevNotes <anushkamadusanka2016@gmail.com>
2019-06-19 15:26:38 -0700

    Challenge 1 answers

--- app/src/main/java/com/anushka/circles/MainActivity.kt
+++ app/src/main/java/com/anushka/circles/MainActivity.kt
@@ -10,11 +10,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val subject = "Maths"
-        var marks = 80
-        var average = 76.8
+//        val subject = "Maths"
+//        var marks = 80
+//        var average = 76.8
+//
+//        Toast.makeText(applicationContext,"I got $marks for $subject . My Average was $average ",Toast.LENGTH_LONG).show()
 
-        Toast.makeText(applicationContext,"I got $marks for $subject . My Average was $average ",Toast.LENGTH_LONG).show()
+        val name = "Anushka"
+        var distance = 8
+
+        Toast.makeText(applicationContext," $name ran $distance kilometers today", Toast.LENGTH_LONG).show()
+
+
 
     }
 }
