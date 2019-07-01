d67a94e79b3be041194459b3dd31401faa4ae04e
AppDevNotes <anushkamadusanka2016@gmail.com>
2019-07-01 06:45:59 -0700

    UI updated

--- app/src/main/java/com/anushka/circles/MainActivity.kt
+++ app/src/main/java/com/anushka/circles/MainActivity.kt
@@ -10,18 +10,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val subject : String
-        var marks : Int
-        var average : Double = 76.8
-        marks = 80
-        subject = "Maths"
-
-        Toast.makeText(applicationContext,getMessage(subject,marks,average),Toast.LENGTH_LONG).show()
-
     }
 
-    private fun getMessage(mySubject : String, myMarks : Int, myAvg : Double) : String {
-
-        return "I got $myMarks for $mySubject . My Average was $myAvg "
-    }
 }
