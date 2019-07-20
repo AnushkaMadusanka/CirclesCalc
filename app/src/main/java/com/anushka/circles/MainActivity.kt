4fa27a130a7e26cfad295daebfe889bf6016c9d7
AppDevNotes <anushkamadusanka2016@gmail.com>
2019-07-20 14:53:05 -0700

    Use Kotlin Android Extensions

--- app/src/main/java/com/anushka/circles/MainActivity.kt
+++ app/src/main/java/com/anushka/circles/MainActivity.kt
@@ -2,10 +2,8 @@ package com.anushka.circles
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,43 +11,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val radiusEditText = findViewById<EditText>(R.id.radiusEditText)
-        val introTextView = findViewById<TextView>(R.id.introTextView)
-        val resultTextView = findViewById<TextView>(R.id.resultTextView)
         val circlesCalc = CirclesCalc()
 
-        //calculate circumference
-        val circumButton = findViewById<Button>(R.id.circumferenceButton)
-        circumButton.setOnClickListener {
-
-            if(radiusEditText.text.toString().isEmpty()){
-
-                Toast.makeText(this,"Empty radius",Toast.LENGTH_LONG).show()
+        circumferenceButton.setOnClickListener {
+            if (radiusEditText.text.toString().isEmpty()) {
+                Toast.makeText(this, "Empty radius", Toast.LENGTH_LONG).show()
             } else {
-
                 val input = radiusEditText.text.toString()
                 val circumference = circlesCalc.getCircumference(input.toDouble())
                 introTextView.text = getString(R.string.circumference_text)
                 resultTextView.text = circumference.toString().plus(" cm")
-
             }
         }
-            //calculate area
-            val areaButton = findViewById<Button>(R.id.areaButton)
-            areaButton.setOnClickListener {
-                if(radiusEditText.text.toString().isEmpty()){
 
-                    Toast.makeText(this,"Empty radius",Toast.LENGTH_LONG).show()
-                } else {
-
-                    val input = radiusEditText.text.toString()
-                    val area = circlesCalc.getArea(input.toDouble())
-                    introTextView.text = getString(R.string.area_text)
-                    resultTextView.text = area.toString().plus(" cm\u00B2")
-                }
-
+        areaButton.setOnClickListener {
+            if (radiusEditText.text.toString().isEmpty()) {
+                Toast.makeText(this, "Empty radius", Toast.LENGTH_LONG).show()
+            } else {
+                val input = radiusEditText.text.toString()
+                val area = circlesCalc.getArea(input.toDouble())
+                introTextView.text = getString(R.string.area_text)
+                resultTextView.text = area.toString().plus(" cm\u00B2")
             }
-
         }
-
     }
+}
